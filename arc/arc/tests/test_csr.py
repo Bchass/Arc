8036d7749b6d7b2c7792a0813e59c6ebaadd6cc9
BChass <34097574+Bchass@users.noreply.github.com>
2024-05-16 22:36:37 -0400

    start of test cases for csr_matrix

---
+++ arc/arc/tests/test_csr.py
@@ -0,0 +1,30 @@
+import pytest
+from arc.arc.csr import csr_matrix
+from arc.arc.data_types import *
+
+
+def test_default_constructor():
+    test_input_matrix = csr_matrix(3, 3)
+    assert test_input_matrix.rows == 3
+    assert test_input_matrix.cols == 3
+    expected_matrix = [[0] * 3 for _ in range(3)]
+
+    assert test_input_matrix.matrix == expected_matrix
+
+
+def test_all():
+    row = [0, 0, 1, 2, 2, 2]
+    col = [0, 2, 2, 0, 1, 2]
+    data = [1, 2, 3, 4, 5, 6]
+    test_input_matrix = csr_matrix(3, 3, data=data, row=row, col=col)
+    expected_matrix = [[1, 0, 2], [0, 0, 3], [4, 5, 6]]
+
+    assert test_input_matrix.matrix == expected_matrix
+
+
+def test_data():
+    data = [1, 2, 3, 4, 5, 6]
+    test_input_matrix = csr_matrix(3, 3, data=data)
+    expected_matrix = [[1, 2, 3], [4, 5, 6], [0, 0, 0]]
+
+    assert test_input_matrix.matrix == expected_matrix
