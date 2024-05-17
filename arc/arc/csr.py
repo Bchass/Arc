c629884f21da684c91cd795ea52b0333bab9faa9
BChass <34097574+Bchass@users.noreply.github.com>
2024-05-16 22:12:29 -0400

    toarray and dtype

--- arc/arc/csr.py
+++ arc/arc/csr.py
@@ -35,10 +35,10 @@ class csr_matrix:
 
     def __str__(self):
         if self.dtype is not None:
-            # dtype_str = self.dtype.__name__
+            dtype_str = self.dtype.__name__
             matrix_str = "\n".join(
                 ["[" + ", ".join(map(str, row)) + "]" for row in self.matrix])
-            # matrix_str = matrix_str + ", dtype=" + dtype_str
+            matrix_str = matrix_str + ", dtype=" + dtype_str
         else:
             matrix_str = "\n".join(
                 ["[" + ", ".join(map(str, row)) + "]" for row in self.matrix])
@@ -48,3 +48,12 @@ class csr_matrix:
         if self.matrix is None:
             return "None"
         return str(self)
+
+    def toarray(self):
+
+        array = self.matrix[:]
+        if self.dtype is not None:
+            dtype_str = self.dtype.__name__
+            return [row[:] for row in array], dtype_str
+        else:
+            return [row[:] for row in array]
