a0987e39417a80b1055bd36de50c20a51501a5dc
BChass <34097574+Bchass@users.noreply.github.com>
2024-05-18 10:34:18 -0400

    separate function to load BLAS

--- arc/arc/linear_alg/BLAS.py
+++ arc/arc/linear_alg/BLAS.py
@@ -8,56 +8,61 @@ For implementation: https://developer.apple.com/documentation/accelerate
 
 class arc_BLAS:
 
+    def load_BLAS():
+
+        # Load the Accelerate framework
+        return ctypes.CDLL(
+            '/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/libBLAS.dylib')
+
     @staticmethod
     def sdot(x, y):
 
-        # Load the Accelerate framework
-        libblas = ctypes.CDLL(
-            '/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/libBLAS.dylib')
+        blas_lib = arc_BLAS.load_BLAS()
 
         # Arguments and return types
-        libblas.cblas_sdot.argtypes = [ctypes.c_int, ctypes.POINTER(
+        blas_lib.cblas_sdot.argtypes = [ctypes.c_int, ctypes.POINTER(
             ctypes.c_float), ctypes.c_int, ctypes.POINTER(ctypes.c_float), ctypes.c_int]
 
-        libblas.cblas_sdot.restype = ctypes.c_float
+        blas_lib.cblas_sdot.restype = ctypes.c_float
 
         n = len(x)
         # Convert Python lists to C arrays
         x_arr = (ctypes.c_float * n)(*x)
         y_arr = (ctypes.c_float * n)(*y)
-        result = libblas.cblas_sdot(n, x_arr, 1, y_arr, 1)
+        result = blas_lib.cblas_sdot(n, x_arr, 1, y_arr, 1)
         rounded = round(result, 2)
         return rounded
 
     @staticmethod
     def sdsdot(x, y):
-        libblas = ctypes.CDLL(
-            '/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/libBLAS.dylib')
 
-        libblas.cblas_sdsdot.argtypes = [ctypes.c_int, ctypes.POINTER(
+        blas_lib = arc_BLAS.load_BLAS()
+
+        blas_lib.cblas_sdsdot.argtypes = [ctypes.c_int, ctypes.POINTER(
             ctypes.c_double), ctypes.c_int, ctypes.POINTER(ctypes.c_double), ctypes.c_int]
 
-        libblas.cblas_sdsdot.restype = ctypes.c_double
+        blas_lib.cblas_sdsdot.restype = ctypes.c_double
 
         n = len(x)
         x_arr = (ctypes.c_double * n)(*x)
         y_arr = (ctypes.c_double * n)(*y)
         alpha = [a * b for a, b in zip(x, y)]
-        result = libblas.cblas_sdsdot(ctypes.c_int(n), x_arr, 1, y_arr, 1) + sum(alpha)
+        result = blas_lib.cblas_sdsdot(
+            ctypes.c_int(n), x_arr, 1, y_arr, 1) + sum(alpha)
         rounded = round(result, 2)
         return rounded
 
     @staticmethod
     def sasum(x):
-        libblas = ctypes.CDLL(
-            '/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/libBLAS.dylib')
 
-        libblas.cblas_sdot.argtypes = [ctypes.c_int, ctypes.POINTER(
+        blas_lib = arc_BLAS.load_BLAS()
+
+        blas_lib.cblas_sdot.argtypes = [ctypes.c_int, ctypes.POINTER(
             ctypes.c_float), ctypes.c_int]
 
-        libblas.cblas_sasum.restype = ctypes.c_float
+        blas_lib.cblas_sasum.restype = ctypes.c_float
 
         n = len(x)
         x_arr = (ctypes.c_float * n)(*x)
-        result = libblas.cblas_sasum(n, x_arr, 1)
+        result = blas_lib.cblas_sasum(n, x_arr, 1)
         return result
