10b5105d99815726475e0925c53fb739e1b7211f
BChass <34097574+Bchass@users.noreply.github.com>
2024-05-18 17:39:14 -0400

    more test cases

--- arc/matrices/tests/test_csr.py
+++ arc/matrices/tests/test_csr.py
@@ -30,6 +30,30 @@ def test_data():
     assert test_input_matrix.matrix == expected_matrix
 
 
+def test_get_cols():
+    test_input_matrix = csr_matrix(3, 3).get_cols()
+
+    expected_result = 3
+
+    assert test_input_matrix == expected_result
+
+
+def test_get_rows():
+    test_input_matrix = csr_matrix(3, 3).get_rows()
+
+    expected_result = 3
+
+    assert test_input_matrix == expected_result
+
+
+def test_get_size():
+    test_input_matrix = csr_matrix(3, 3).get_size()
+
+    expected_result = 9
+
+    assert test_input_matrix == expected_result
+
+
 def test_nnz():
     row = [0, 0, 1, 2, 2, 2]
     col = [0, 2, 2, 0, 1, 2]
@@ -39,3 +63,29 @@ def test_nnz():
     expected_result = 6
 
     assert test_input_matrix == expected_result
+
+
+def test_multiplication():
+    row1 = [0, 0, 1, 1, 2, 2]
+    col1 = [0, 1, 0, 1, 0, 1]
+    data1 = [1, 2, 3, 4, 5, 6]
+    A = csr_matrix(3, 2, data=data1, row=row1, col=col1)
+
+    row2 = [0, 0, 1, 1]
+    col2 = [0, 1, 0, 1]
+    data2 = [7, 8, 9, 10]
+    D = csr_matrix(2, 2, data=data2, row=row2, col=col2)
+
+    assert (A[0, :] == [1, 2] and (A[1, :] == [3, 4] and (A[2, :] == [5, 6])))
+    assert (D[0, :] == [7, 8] and (D[1, :] == [9, 10]))
+
+    result_matrix = A.multiply(D)
+
+    assert (result_matrix[0, :] == [25, 28] and (
+        result_matrix[1, :] == [57, 64] and (result_matrix[2, :] == [89, 100])))
+
+    with pytest.raises(ValueError):
+        A = csr_matrix(3, 3)
+        D = csr_matrix(3, 3)
+
+        result_matrix = A.multiply(D)
